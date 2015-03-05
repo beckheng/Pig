d92f1359082d0fb77ff978fa1fcfb89fbb90099a
Beckheng Lam <beckheng@gmail.com>
2015-03-05 23:56:54 +0800

    first test

--- Pig/main.cpp
+++ Pig/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 
+#include "src/pigrequest.pb.h"
+
 using namespace std;
+using namespace Pig;
 
 int main()
 {
     cout << "Pig Run!" << endl;
+
+    PigRequest *request = new PigRequest();
+    string action = "hello world";
+    request->set_action(action);
+    request->set_body("helloobject");
+
+    cout << request->SerializeAsString() << endl;
+
+    delete request;
+
     return 0;
 }
-
